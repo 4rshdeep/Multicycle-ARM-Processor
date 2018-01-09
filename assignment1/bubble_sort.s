a129045cb5745c964f85655fb0409152362de179
ansh2233 <ansh.sapra2233@gmail.com>
2018-01-09 15:48:48 +0530

    Update bubble_sort.s

--- assignment1/bubble_sort.s
+++ assignment1/bubble_sort.s
@@ -3,33 +3,30 @@
 .text 
 .global main
 							
-main:	ldr r1,=P			@ r1 <- start address do not update!
-		add r3, r1, #24		@ r3 <- prefinal address
+main:		ldr r1,=P			@ r1 <- start address do not update!
+		add r3, r1, #28		@ r3 <- prefinal address
+		add r9, r1, #24		@ r9 <- prefinal address
 		mov r10, #-1  		@ r10 stores numswaps
-
 		mov r5, r1          @ store starting address in r5
 
-for:	cmp r5, r3			@ have we reached prefinal position?
+for:		sub r3, r3, #4
+		cmp r5, r9			@ have we reached prefinal position?
 		bgt end				@ exit if address equal to last word
-
 		add r5, r5, #4
 		mov r4, r1          @ store first address in second pointer
 		
-for_2:	cmp r4, r3
+for_2:		cmp r4, r3
 		bgt for
-
 		ldr r7, [r4]		
 		mov r0, r4			@ save address of previous (r7)
-
 		add r4, r4, #4		@ put address of next in r4 for storing later
-
 		ldr r8, [r4]
 		cmp r7, r8
 		bgt swap
 		b for_2
 
 @ display sorted values and return control to simulator
-end:	mov r0, r1
+end:		mov r0, r1
 		ldr r1, [r0]
 		ldr r2, [r0, #4]
 		ldr r3, [r0, #8]
@@ -41,10 +38,11 @@ end:	mov r0, r1
 
 		swi SWI_Exit
 
-swap:	str r8, [r0]  		@ change value in address
+swap:		str r8, [r0]  		@ change value in address
 		str r7, [r4]		@ str goes like Ri -> [Rj]
 		b for_2
 
 .data
-	P: .word 4, 10, 6, 1, 3, 23, 67, 35
+	@P: .word 4, 10, 6, 1, 3, 23, 67, 35
+	P: .word 67, 35, 23, 10, 6, 4, 3, 1
 .end
