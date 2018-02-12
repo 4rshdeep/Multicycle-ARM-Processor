71dd9aad2776a12e72f1e0ca8f8b8d392bb5bca7
Arshdeep Singh <arshdeepiitd@gmail.com>
2018-02-12 13:33:24 +0530

    alu start

--- lab4/lab4.vhd
+++ lab4/lab4.vhd
@@ -6,9 +6,27 @@ entity ALU is
 port(
 		a : in std_logic_vector(31 downto 0);
 		b : in std_logic_vector(31 downto 0);
-		results : out std_logic_vector(31 downto 0);
-		nzvf : out std_logic_vector(3 downto 0)
+		result : out std_logic_vector(31 downto 0);
+		nzvf : out std_logic_vector(3 downto 0);
+		carry : in std_logic;
+		opcode : in std_logic_vector(3 downto 0)
 	);
 end entity;
 
-arc
+architecture behaviour_alu of ALU is
+
+begin
+	p1 : process( a, b, carry, opcode )
+	begin
+		case( operation ) is
+
+			when "0000" => result(31 downto 0) <= a and b;
+
+			when others =>
+
+		end case ;
+
+	end process ; -- p1
+
+
+end behaviour_alu ; -- behaviour_alu
