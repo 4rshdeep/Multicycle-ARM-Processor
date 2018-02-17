fb8906f911ca2639dc211040eae2f4fb45d2c462
Arshdeep Singh <arshdeepiitd@gmail.com>
2018-02-17 12:19:48 +0530

    added carry in shifter

--- lab4/shifter.vhd
+++ lab4/shifter.vhd
@@ -27,12 +27,28 @@ begin
 	-- 10 -> ror
 	case( opcode ) is
 
-		when "00" => result <= shift_left(unsigned(op), shift_int);
-		when "01" => result <= shift_right(unsigned(op), shift_int);
-		when "11" => result <= shift_right(signed(op), shift_int);
-		when "10" => result <= op(shift_int-1 downto 0) & op(31 downto shift_int);
-
+		when "00" =>
+				result <= shift_left(unsigned(op), shift_int);
+				if shift_int /= 0 then
+					carry <= op(32-shift_int);
+				end if ;
+		when "01" =>
+				result <= shift_right(unsigned(op), shift_int);
+				if shift_int /= 0 then
+					carry <= op(shift_int-1);
+				end if ;
+		when "11" =>
+				result <= shift_right(signed(op), shift_int);
+				if shift_int /= 0 then
+					carry <= op(shift_int-1);
+				end if ;
+		when "10" =>
+				result <= op(shift_int-1 downto 0) & op(31 downto shift_int);
+				if shift_int /= 0 then
+					carry <= op(shift_int-1);
+				end if ;
 		when others =>
+				result <= (others => '0');
 
 	end case ;
 
