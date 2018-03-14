f647fc119dd09f5c8ef9f26290d4f2334efefbcd
Arshdeep Singh <arshdeepiitd@gmail.com>
2018-03-14 13:14:22 +0530

    added alu controller

--- arm_processor/Actrl.vhd
+++ arm_processor/Actrl.vhd
@@ -17,4 +17,22 @@ begin
 
 	op <= IR(24 downto 21);
 
+	if IR(27 downto 26) = "01" then -- ldr, str
+        if IR(23) = '1' then
+            op <= "0100";
+        else
+            op <= "0010";
+        end if ;
+    elsif IR(27 downto 25) = "101" then -- b, bl
+        op <= "0100";
+    elsif IR(27 downto 23) = "00001" and IR(7 downto 4) = "1001" then --mul and mla confirm condition!
+        if IR(21) = '0' then --mul
+            op <= "1101";
+        else                -- mla
+            op <= "0100";
+        end if ;
+    else
+        op <= IR(24 downto 21);  -- normal alu operations
+	end if ;
+
 end architecture ; -- arch
