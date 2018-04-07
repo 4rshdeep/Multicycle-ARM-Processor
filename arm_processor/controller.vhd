fc1208b912c431d6bde2f46377c99f1717fa5f78
Arshdeep Singh <arshdeepiitd@gmail.com>
2018-04-07 20:11:46 +0530

    minor changes

--- arm_processor/controller.vhd
+++ arm_processor/controller.vhd
@@ -9,6 +9,7 @@ package pkg is
 	type opcode is (AND, EOR, SUB, RSB, ADD, SBC, RSC, TST, TEQ, SMP, SMN, ORR, MOV, BIC, MVN);
 	type DP_type is (IMM, SHIFT_IMM, SHIFT_REG);
 	type DT_type is (HALF_WORD, WORD);
+	type state_type is (s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16, s17, s18, s19, s20, s21, s22, s23, s24, s25);
 end package ;
 
 package body pkg is
@@ -45,6 +46,13 @@ entity controller is
     p2m_offset : out std_logic_vector(1 downto 0);
     shifter_opcode: out std_logic_vector(1 downto 0);
 	RWAD 	: out std_logic;
+------------------------------------
+	state : in state_type
+	instruction : out std_logic_vector(31 downto 0) ;
+	op 	: in opcode;
+	reset : in std_logic;
+-- predicate
+	p : in std_logic;
 
 -------------------------------------------------------------
 	Flags	: in std_logic_vector(3 downto 0);
@@ -55,7 +63,67 @@ end controller ;
 architecture arch of controller is
 
 begin
+	if (reset = '1') then
+		
+	else
+		case(state) is
+			when s0  =>  ;
 
+			when s1  =>  ;
+			
+			when s2  =>  ;
+			
+			when s3  =>  ;
+			
+			when s4  =>  ;
+			
+			when s5  =>  ;
+			
+			when s6  =>  ;
+			
+			when s7  =>  ;
+			
+			when s8  =>  ;
+			
+			when s9  =>  ;
+			
+			when s10 =>  ;
+			
+			when s11 =>  ;
+			
+			when s12 =>  ;
+			
+			when s13 =>  ;
+			
+			when s14 =>  ;
+			
+			when s15 =>  ;
+			
+			when s16 =>  ;
+			
+			when s17 =>  ;
+			
+			when s18 =>  ;
+			
+			when s19 =>  ;
+			
+			when s20 =>  ;
+			
+			when s21 =>  ;
+			
+			when s22 =>  ;
+			
+			when s23 =>  ;
+			
+			when s24 =>  ;
+			
+			when s25 =>  ;
+
+			when others =>
+
+
+		end case;
+	end if;
 
 
 end architecture ; -- arch
